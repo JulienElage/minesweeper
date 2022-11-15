2573230d2fdd1b9023dc081b01ec6652c176c8cc
JulienElage <julien.elage2@gmail.com>
2022-11-15 17:33:04 +0100

    version beta demineur

--- main.js
+++ main.js
@@ -2,10 +2,9 @@
 const easyDifficultyButton = document.getElementById('easyDifficultyButton');
 const normalDifficultyButton = document.getElementById('normalDifficultyButton');
 const hardDifficultyButton = document.getElementById('hardDifficultyButton');
-
-let difficulty = 0;
-var tileArrayHidden;
 const canvas = document.querySelector('canvas')
+var hiddenTileArray;
+let difficulty = 0;
 let ctx = canvas.getContext('2d');
 
 class Tile {
@@ -28,28 +27,33 @@ class Position {
 //L'utilisateur choisis la difficulté
 easyDifficultyButton.addEventListener('click', () => {
     difficulty = 7;
-    tileArrayDisplay(difficulty);
-    tileArrayHidden = createHiddenTileArray(difficulty);
-    placeBombs(difficulty);
+    hiddenTileArray = createHiddenTileArray();
+    displayTiles();
+    placeBombs();
+    setTilesNumber();
 })
+
 //L'utilisateur choisis la difficulté
 normalDifficultyButton.addEventListener('click', () => {
     difficulty = 10;
-    tileArrayDisplay(difficulty);
-    tileArrayHidden = createHiddenTileArray(difficulty);
-    placeBombs(difficulty);
+    hiddenTileArray = createHiddenTileArray();
+    displayTiles();
+    placeBombs();
+    setTilesNumber();
 })
+
 //L'utilisateur choisis la difficulté
 hardDifficultyButton.addEventListener('click', () => {
     difficulty = 15;
-    tileArrayDisplay(difficulty);
-    tileArrayHidden = createHiddenTileArray(difficulty);
-    placeBombs(difficulty);
+    hiddenTileArray = createHiddenTileArray();
+    displayTiles();
+    placeBombs();
+    setTilesNumber();
 })
 
 //Fonction qui affiche un nombre d'images de tuiles en fonction de la difficultée.
-function tileArrayDisplay(difficulty) {
-    
+function displayTiles() {
+
     ctx.canvas.width  = difficulty*45;
     ctx.canvas.height = difficulty*48;
     for (let x = 0; x < difficulty; x++) {
@@ -60,12 +64,11 @@ function tileArrayDisplay(difficulty) {
             ctx.drawImage(imageTile, x*45, y*48, 45, 48,);
         }, false);
         }
-
     }
 }
 
 //Fonction qui créée un tableau 7*7 ou 10*10 ou 15*15 en fonction de la difficultée choisie et l'initialise à 0 partout.
-function createHiddenTileArray(difficulty) {
+function createHiddenTileArray() {
     
     var tileArrayHidden = new Array(difficulty)
 
@@ -81,71 +84,92 @@ function createHiddenTileArray(difficulty) {
     } 
     console.log(tileArrayHidden);
     return tileArrayHidden;
-   
+
 }
 
 //Fonction qui place le nombre 10 (qui représente les bombes) à un emplacement aléatoire dans le tableau. On répète l'opération en fonction de la difficultée.
-function placeBombs(difficulty) {
-    for (let i = 0; i < difficulty*2; i++){
+function placeBombs() {
+
+    for (let i = 0; i < difficulty; i++){
         a = randomInt(difficulty-1);
         b = randomInt(difficulty-1);
-        if(tileArrayHidden[a][b].value != 10){
-            tileArrayHidden[a][b].value = 10;
+        if(hiddenTileArray[a][b].value != 10){
+            hiddenTileArray[a][b].value = 10;
         } else i--;
     }
     //console.log(tileArrayHidden);
 }
+
+//Fonction qui associe au tuiles leur numéro, en fonction des bombes adjascentes (+1 par bombe)
+function setTilesNumber() {
+
+    for(let i = 0 ; i < difficulty ; i++){
+
+        for(let j = 0 ; j < difficulty ; j++){
+
+                if(i+1 <= difficulty-1 && hiddenTileArray[i+1][j].value >= 10) {
+                    hiddenTileArray[i][j].value += 1
+                }
+                    
+                if(i+1 <= difficulty-1 && j+1 <= difficulty-1 && hiddenTileArray[i+1][j+1].value >= 10) {
+                    hiddenTileArray[i][j].value += 1
+                }
+                    
+                if(i+1 <= difficulty-1 && j-1 >= 0 && hiddenTileArray[i+1][j-1].value >= 10) {
+                    hiddenTileArray[i][j].value += 1
+                }
+                    
+                if(i-1 >= 0 && hiddenTileArray[i-1][j].value >= 10) {
+                    hiddenTileArray[i][j].value += 1
+                }
+                    
+                if(i-1 >= 0 && j-1 >= 0 && hiddenTileArray[i-1][j-1].value >= 10){
+                    hiddenTileArray[i][j].value += 1
+                }
+                    
+                if(i-1 >= 0 && j+1 <= difficulty-1 && hiddenTileArray[i-1][j+1].value >= 10) {
+                    hiddenTileArray[i][j].value += 1
+                }
+                    
+                if(j+1 <= difficulty-1 && hiddenTileArray[i][j+1].value >= 10) {
+                     hiddenTileArray[i][j].value += 1
+                }
+                   
+                if(j-1 >= 0 && hiddenTileArray[i][j-1].value >= 10) {
+                     hiddenTileArray[i][j].value += 1
+                }
+  
+            }
+
+    }
+    console.log(hiddenTileArray);
+}
+
 //L'utilisateur fait un clique gauche sur une tuile
 canvas.addEventListener('click', function(e) {
-    var position = new Position
+
+    var position = new Position;
     position = getPosition(canvas, e);
-    x = position.x
-    y = position.y
-    userLeftClickedOnTile(x,y)
+    x = Math.floor(position.x/45);
+    y = Math.floor(position.y/48);
+    userLeftClickedOnTile(x,y);
 })
+
 //L'utilisateur fait un clique droit sur une tuile
 canvas.addEventListener('contextmenu', function(e) {
-    var position = new Position
+
+    var position = new Position;
     position = getPosition(canvas, e);
-    x = position.x
-    y = position.y
-    userRightClickedOnTile(x,y)
+    x = Math.floor(position.x/45);
+    y = Math.floor(position.y/48);
+    userRightClickedOnTile(x,y);
     e.preventDefault();
+
 })
 
-function userLeftClickedOnTile(x,y){
-    if(tileArrayHidden[Math.floor(x/45)][Math.floor(y/48)].isDiscovered == false && tileArrayHidden[Math.floor(x/45)][Math.floor(y/48)].isMarked == 0){
-        var imageTile = document.createElement("img");
-        imageTile.src = "./image/bombTile.jpg";
-        var imageDiscoveredTile = document.createElement("img");
-        imageDiscoveredTile.src = "./image/discoveredTile.jpg";
-
-        if(tileArrayHidden[Math.floor(x/45)][Math.floor(y/48)].value == 10){
-                imageTile.addEventListener('load', function(){
-                ctx.drawImage(imageTile, Math.floor(x/45)*45, Math.floor(y/48)*48, 45, 48,);
-                }, false);
-                console.log("perdu");
-        } 
-        else {
-            imageTile.addEventListener('load', function(){
-            ctx.drawImage(imageDiscoveredTile, Math.floor(x/45)*45, Math.floor(y/48)*48, 45, 48,);
-            }, false);
-        }
-        tileArrayHidden[Math.floor(x/45)][Math.floor(y/48)].isDiscovered = true;
-    }
-}
-
-function userRightClickedOnTile(x,y){
-    if(tileArrayHidden[Math.floor(x/45)][Math.floor(y/48)].isDiscovered == false && tileArrayHidden[Math.floor(x/45)][Math.floor(y/48)].isMarked == 0){
-        var imageMarkedTile = document.createElement("img");
-        imageMarkedTile.src = "./image/markedTile.jpg";
-        imageMarkedTile.addEventListener('load', function(){
-            ctx.drawImage(imageMarkedTile, Math.floor(x/45)*45, Math.floor(y/48)*48, 45, 48,);
-        }, false);
-        tileArrayHidden[Math.floor(x/45)][Math.floor(y/48)].isMarked = 1;
-    }
-}
+//fonction qui va renvoyer la position du curseur en x,y dans un tableau
 function getPosition(canvas, event) {
+
     const rect = canvas.getBoundingClientRect()
     const x = event.clientX - rect.left
     const y = event.clientY - rect.top
@@ -154,11 +178,115 @@ function getPosition(canvas, event) {
     position.y = y
     return position;
 
-   
+}
+
+//Fonction qui va découvrir la tuile, si elle ne l'est pas déjà, et si elle n'est pas marquée (drapeau ou '?').
+function userLeftClickedOnTile(x,y){
+
+    if(hiddenTileArray[x][y].isDiscovered == false && hiddenTileArray[x][y].isMarked == 0){
+        var imageBombTile = document.createElement("img");
+        imageBombTile.src = "./image/bombTile.jpg";
+        var imageDiscoveredTile = document.createElement("img");
+        imageDiscoveredTile.src = "./image/discoveredTile.jpg";
+
+        if(hiddenTileArray[x][y].value >= 10){
+                imageBombTile.addEventListener('load', function(){
+                ctx.drawImage(imageBombTile, x*45, y*48, 45, 48,);
+                }, false);
+                console.log("perdu");
+        } 
+        else {
+            imageDiscoveredTile.addEventListener('load', function(){
+            ctx.drawImage(imageDiscoveredTile, x*45, y*48, 45, 48,);
+            let value = hiddenTileArray[x][y].value;
+            ctx.font = "10px Arial";
+            ctx.textAlign = "center";
+            if(value != 0){
+            ctx.fillText(value, x*45+22.5,y*48+24);
+            }
+            }, false);
+        }
+        hiddenTileArray[x][y].isDiscovered = true;
+        if(hiddenTileArray[x][y].value == 0) {
+             tileIsSetToZero(x,y)
+        }
+        
+    }
+}
+
+function tileIsSetToZero(x,y) {
+
+    if(x+1 <= difficulty-1) {
+        userLeftClickedOnTile(x+1,y);
+    }
+        
+    if(x+1 <= difficulty-1 && y+1 <= difficulty-1) {
+        userLeftClickedOnTile(x+1,y+1);
+    }
+        
+    if(x+1 <= difficulty-1 && y-1 >= 0) {
+        userLeftClickedOnTile(x+1,y-1);
+    }
+        
+    if(x-1 >= 0) {
+        userLeftClickedOnTile(x-1,y);
+    }
+        
+    if(x-1 >= 0 && y-1 >= 0){
+        userLeftClickedOnTile(x-1,y-1);
+    }
+        
+    if(x-1 >= 0 && y+1 <= difficulty-1) {
+        userLeftClickedOnTile(x-1,y+1);
+    }
+        
+    if(y+1 <= difficulty-1) {
+        userLeftClickedOnTile(x,y+1);
+    }
+       
+    if(y-1 >= 0) {
+        userLeftClickedOnTile(x,y-1);
+    }
+
+}
+
+//Fonction qui marque la tuile avec d'abord un drapeau puis un '?', puis elle revient à la normale.
+function userRightClickedOnTile(x,y){
+    switch (true){
+
+        case hiddenTileArray[x][y].isDiscovered == false && hiddenTileArray[x][y].isMarked == 0:
+            var imageMarkedTile = document.createElement("img");
+            imageMarkedTile.src = "./image/markedTile.jpg";
+            imageMarkedTile.addEventListener('load', function(){
+                ctx.drawImage(imageMarkedTile, x*45, y*48, 45, 48,);
+            }, false);
+            hiddenTileArray[x][y].isMarked = 1;
+            break;
+
+        case hiddenTileArray[x][y].isDiscovered == false && hiddenTileArray[x][y].isMarked == 1:
+            var imageQuestionningTile = document.createElement("img");
+            imageQuestionningTile.src = "./image/questionningTile.jpg";
+            imageQuestionningTile.addEventListener('load', function(){
+                ctx.drawImage(imageQuestionningTile, x*45, y*48, 45, 48,);
+            }, false);
+            hiddenTileArray[x][y].isMarked = 2;
+            break;
+
+        case hiddenTileArray[x][y].isDiscovered == false && hiddenTileArray[x][y].isMarked == 2:
+            var imageTile = document.createElement("img");
+            imageTile.src = "./image/tile.jpg";
+            imageTile.addEventListener('load', function(){
+                ctx.drawImage(imageTile, x*45, y*48, 45, 48,);
+            }, false);
+            hiddenTileArray[x][y].isMarked = 0;
+            break;
+    }
+
 }
 
 //Fonction qui renvoie un nombre aléatoire (pas besoin d'un minimum, car c'est 0 dans notre cas).
-function randomInt(max)
-{
+function randomInt(max) {
+
     return Math.floor(Math.random() * (max + 1));
+
 }
